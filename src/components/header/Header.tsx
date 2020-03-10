64a1c4f80e05e2d8e1ab57be72dd09a9562b89e9
BenjaminParment <parment.benjamin@gmail.com>
2020-03-10 15:58:08 +0000

    fixes tests, adds absolute imports, creates header (#2)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,16 @@
+import "./Header.scss";
+
 import React from "react";
 
 class Header extends React.Component {
     render = () => (
-        <div>Blyat, render me a header</div>
+        <div className="header-parent-container">
+            <div className="left-container">
+                <span className="header-title">Title</span>
+                <div className="header-buttons">Buttons</div>
+            </div>
+            <div className="header-language-select">Language Select</div>
+        </div>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import Home from "../components/home/Home";
+import "./index.scss";
+
+import Home from "components/home/Home";
 import React from "react";
 
 const IndexPage = () => (
   <Home/>
 )
 
-export default IndexPage
+export default IndexPage;
