b0e48fe8b81506b8e3912b0a6c759dc5fe1e6f1c
BenjaminParment <parment.benjamin@gmail.com>
2020-03-10 14:33:05 +0000

    deletes all pre generated class, creates home component where header, footer and main content will be added (#1)

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render = () => (
+        <div>Blyat, render me a Footer</div>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component {
+    render = () => (
+        <div>Blyat, render me a header</div>
+    )
+}
+
+export default Header;
