541edbd2a0982b441d7c5125144cb838a745ddb2
BenjaminParment <parment.benjamin@gmail.com>
2020-03-11 15:05:29 +0000

    create footer (#5)
    * creates footer, changes some styling from header and main body

---
+++ src/components/content/Content.tsx
@@ -0,0 +1,13 @@
+import "./Content.scss"
+
+import React from "react"
+
+const Content: React.FC = () => {
+    return (
+        <div className="body-parent-container">
+            <span>Hello I am the body</span>
+        </div>
+    )
+}
+
+export default Content
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,17 +1,16 @@
-import Footer from '../footer/Footer';
-import Header from '../header/Header';
-import React from "react";
+import Content from "components/content/Content"
+import Footer from "components/footer/Footer"
+import Header from "components/header/Header"
+import React from "react"
 
 class Home extends React.Component {
     render = () => (
         <div className="home-parent-container">
             <Header />
-            <div className="main-content">
-                Yes, thats where the main content goes ey
-            </div>
+            <Content />
             <Footer />
         </div>
     )
 }
 
-export default Home;
+export default Home
