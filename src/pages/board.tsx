36cdbbe4f1836ae8529d7190b775a1956a5c9317
BenjaminPar <parment.benjamin@gmail.com>
2020-03-13 16:47:06 +0000

    creates reusable profile display, fills board page

--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,10 +1,36 @@
+import "./page-style/board.scss"
+
+import { FormattedMessage } from "gatsby-plugin-intl"
 import Layout from "components/layout/Layout"
+import { ProfileDescription } from "components/reusables/profile-description/ProfileDescription"
+import { QuestionMarkImage } from "components/images/QuestionMarkImage"
 import React from "react"
 
 const BoardPage: React.FC = () => {
     return (
         <Layout>
-            <span>Activities Page Hey</span>
+            <div className="board-parent-container">
+                <div className="left-container">
+                    <span className="board-title">
+                        <FormattedMessage id="board.title" />
+                    </span>
+                    <ProfileDescription
+                        id="board.paul"
+                        image={<QuestionMarkImage />}
+                    />
+                    <ProfileDescription
+                        id="board.ward"
+                        image={<QuestionMarkImage />}
+                    />
+                    <ProfileDescription
+                        id="board.luc"
+                        image={<QuestionMarkImage />}
+                    />
+                </div>
+                <div className="right-container">
+                    <FormattedMessage id="board.description" />
+                </div>
+            </div>
         </Layout>
     )
 }
