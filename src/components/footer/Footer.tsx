762c2e9cbbf81692c74e7391aec609267142b77a
BenjaminParment <parment.benjamin@gmail.com>
2020-03-18 14:12:52 +0000

    changed footer styling (#18)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,22 +1,14 @@
 import "./Footer.scss"
 
-import { FormattedMessage } from "gatsby-plugin-intl"
 import React from "react"
 
 class Footer extends React.Component {
     render = () => (
         <div className="footer-parent-container">
             <div className="footer-info">
-                <span className="single-info">
-                    <FormattedMessage id={"footer.footer_1"} />
-                </span>
-                <span className="single-info">
-                    <FormattedMessage id={"footer.footer_2"} />
-                </span>
-                <span className="single-info">RSIN 858489995</span>
-                <span className="single-info">
-                    Lekstraat 74-2 1079ET Amsterdam
-                </span>
+                <span className="column">1</span>
+                <span className="column">2</span>
+                <span className="column">3</span>
             </div>
         </div>
     )
