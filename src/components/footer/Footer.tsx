0b6ce8c6cd83505d181a290e7062cb5a1b5b01a6
BenjaminParment <parment.benjamin@gmail.com>
2021-01-17 11:45:53 +0100

    Removes info page
    removes info from footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 import { FormattedMessage } from "gatsby-plugin-intl";
 import React from "react";
 import { Link } from "gatsby-plugin-intl";
-import { ABOUT_PATH, CONTACT_PATH, INFO_PATH } from "shared/routes";
+import { ABOUT_PATH, CONTACT_PATH } from "shared/routes";
 
 class Footer extends React.Component {
     render = () => (
@@ -12,11 +12,6 @@ class Footer extends React.Component {
                 <span className="column">
                     <Link to={ABOUT_PATH}>
                         <FormattedMessage id="footer.left" />
-                    </Link>
-                </span>
-                <span className="column">
-                    <Link to={INFO_PATH}>
-                        <FormattedMessage id="footer.middle" />
                     </Link>
                 </span>
                 <span className="column">
