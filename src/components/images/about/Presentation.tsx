01821c3f2cce2b0e14d33a280090868ffa6e78b1
BenjaminParment <parment.benjamin@gmail.com>
2021-01-17 12:22:29 +0100

    Adds missing mission on info page (#35)

---
+++ src/components/images/about/Presentation.tsx
@@ -0,0 +1,20 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+import { ImageProps } from "../ImageProps";
+import Img from "gatsby-image";
+import React from "react";
+
+export const PresentationImage: React.FC<ImageProps> = ({ className }) => {
+    const data = useStaticQuery(graphql`
+        query {
+            placeholderImage: file(relativePath: { eq: "image7.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 1920, maxHeight: 1920) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `);
+    return <Img fluid={data.placeholderImage.childImageSharp.fluid} className={className} />;
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { MissionImage } from "components/images/about/Mission";
 import { TeamImage } from "components/images/about/Team";
 import { FormattedMessage } from "gatsby-plugin-intl";
+import { PresentationImage } from "components/images/about/Presentation";
 
 const About: React.FC = () => {
     return (
@@ -13,31 +14,46 @@ const About: React.FC = () => {
                 <div className="title">
                     <FormattedMessage id="about.title" />
                 </div>
-                <div className="mission-parent-container">
-                    <div className="single-program">
-                        <div className="top-container">
-                            <div className="single-program-image">
-                                <MissionImage />
+                <div className="outer-container">
+                    <div className="mission-parent-container">
+                        <div className="single-program">
+                            <div className="top-container">
+                                <div className="single-program-image">
+                                    <MissionImage />
+                                </div>
+                                <div className="single-program-title">
+                                    <FormattedMessage id="about.mission.title" />
+                                </div>
                             </div>
-                            <div className="single-program-title">
-                                <FormattedMessage id="about.mission.title" />
+                            <div className="content">
+                                <FormattedMessage id="about.mission.content" />
                             </div>
                         </div>
-                    </div>
-                    <div className="single-program">
-                        <div className="single-program-image">
-                            <TeamImage />
+                        <div className="single-program">
+                            <div className="top-container">
+                                <div className="single-program-image">
+                                    <TeamImage />
+                                </div>
+                                <div className="single-program-title">
+                                    <FormattedMessage id="about.team.title" />
+                                </div>
+                            </div>
+                            <div className="content">
+                                <FormattedMessage id="about.team.content" />
+                            </div>
                         </div>
-                        <div className="single-program-title">
-                            <FormattedMessage id="about.team.title" />
-                        </div>
-                    </div>
-                    <div className="single-program">
-                        <div className="single-program-image">
-                            <TeamImage />
-                        </div>
-                        <div className="single-program-title">
-                            <FormattedMessage id="about.presentation.title" />
+                        <div className="single-program">
+                            <div className="top-container">
+                                <div className="single-program-image">
+                                    <PresentationImage />
+                                </div>
+                                <div className="single-program-title">
+                                    <FormattedMessage id="about.presentation.title" />
+                                </div>
+                            </div>
+                            <div className="content">
+                                <FormattedMessage id="about.presentation.content" />
+                            </div>
                         </div>
                     </div>
                 </div>
